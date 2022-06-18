7b8fc9ad9093f62a51290cc8a52068ffafe12610
pulkit1207 <pulkit1207@gmail.com>
2022-06-18 07:25:45 -0400

    BST

---
+++ src/binarysearchtree/Main.java
@@ -0,0 +1,21 @@
+package binarysearchtree;
+
+public class Main {
+
+    public static void main(String[] args) {
+        BinarySearchTree myBST = new BinarySearchTree();
+
+        myBST.insert(47);
+        myBST.insert(21);
+        myBST.insert(76);
+        myBST.insert(18);
+        myBST.insert(52);
+        myBST.insert(82);
+
+        myBST.insert(27);
+        System.out.println(myBST.root.left.right.value);
+        System.out.println();
+
+        System.out.println(myBST.contains(82));
+    }
+}
