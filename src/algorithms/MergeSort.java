a843188cd30f6742b592a7b87044766df8a127b2
pulkit1207 <pulkit1207@gmail.com>
2022-06-29 18:19:40 -0400

    MergerSort

---
+++ src/algorithms/MergeSort.java
@@ -0,0 +1,54 @@
+package algorithms;
+
+import java.util.Arrays;
+
+public class MergeSort {
+    public static int[] merge(int[] array1, int[] array2) {
+        int[] combined = new int[array1.length + array2.length];
+        int index = 0;
+        int i = 0;
+        int j = 0;
+        while (i < array1.length && j < array2.length) {
+            if (array1[i] < array2[j]) {
+                combined[index] = array1[i];
+                index++;
+                i++;
+            } else {
+                combined[index] = array2[j];
+                index++;
+                j++;
+            }
+        }
+
+        while (i < array1.length) {
+            combined[index] = array1[i];
+            index++;
+            i++;
+        }
+
+        while (j < array2.length) {
+            combined[index] = array2[j];
+            index++;
+            j++;
+        }
+
+        return combined;
+    }
+
+    public static int[] mergeSort(int[] array){
+        if (array.length == 1 ) return array;
+
+        int middle = array.length/ 2;
+
+        int[] leftArray = Arrays.copyOfRange(array, 0, middle);
+        int[] rightArray = Arrays.copyOfRange(array, middle, array.length);
+
+        return merge(mergeSort(leftArray), mergeSort(rightArray));
+
+    }
+
+    public static void main(String[] args) {
+        int[] sortedArray = mergeSort(new int[]{3, 1, 4, 2});
+        System.out.println(Arrays.toString(sortedArray));
+    }
+}
